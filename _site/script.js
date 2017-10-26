e33380d5b1b36d33169c3db6f021fc144a7eee80
JDodd <doddjd94@gmail.com>
2017-10-26 15:24:23 -0400

    added up arrow

--- _site/script.js
+++ _site/script.js
@@ -14,4 +14,4 @@ function topFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera 
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
-Try it Yourself »
+
